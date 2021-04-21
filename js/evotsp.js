c760e14aa9cbd77b0c6d98869452ef664dbdfac7
Cookie Vang <vang2660@morris.umn.edu>
2021-04-21 00:41:57 -0500

    Add ['routeId'] and ['len'] to for loop

--- js/evotsp.js
+++ js/evotsp.js
@@ -111,10 +111,10 @@
     }
 
     function showBestRoute(result){
-        console.log('The best number of routes from the given runId and generation: ', result);
+        console.log('The best routes from the given runId and generation: ', result);
         for (let i = 0; i < result.length; i++){
-            const routeId = result[i].routeId;
-            const length = result[i].length;
+            const routeId = result[i]['routeId'];
+            const length = result[i]['len'];
         
             $('#best-route-list').append(`<br><li>${length} ,(${routeId})</li>`);
         }
